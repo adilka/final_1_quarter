5aa3c70e90821b4fa65f2be0ed7f28fbf0ddcf66
Adil <adil.manakbayev@gmail.com>
2023-03-05 04:33:51 +0600

    еще один коммит

--- Program.cs
+++ Program.cs
@@ -1,26 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-
-class Program {
+﻿class Program {
     static void Main(string[] args) {
-        // Создаем пустой список для хранения строк, длина которых меньше или равна 3 символам
+        
         List<string> shortStrings = new List<string>();
-
-        // Запрашиваем у пользователя количество строк, которые необходимо ввести
-        Console.Write("Введите количество строк: ");
+        
+        Console.Write("Vvedite kolichestvo strok: ");
         int n = int.Parse(Console.ReadLine());
 
-        // Считываем строки и добавляем их в список, если их длина меньше или равна 3 символам
         for (int i = 0; i < n; i++) {
-            Console.Write("Введите строку: ");
+            Console.Write("Vvedite stroku: ");
             string s = Console.ReadLine();
             if (s.Length <= 3) {
                 shortStrings.Add(s);
             }
         }
-
-        // Выводим полученный список на экран
-        Console.WriteLine("Строки, длина которых меньше или равна 3 символам:");
+        Console.WriteLine("Stroki kotorie i<=3:");
         foreach (string s in shortStrings) {
             Console.WriteLine(s);
         }
